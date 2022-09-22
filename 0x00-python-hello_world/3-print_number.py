e3d28449eea8b339959864cf3962ea533e4aff63
rloterh <rloterh@gmail.com>
2022-09-22 05:12:58 +0000

    Update tasks

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-number = 98
-print("{:d} Battery street".format(number))
+num = 3.14587
+print("Float : {:.2f}".format(num))
--- 0x00-python-hello_world/4-print_float.py
+++ 0x00-python-hello_world/4-print_float.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-number = 3.14159
-print("Float: {:.2f}".format(number))
+num = 98
+print("{:d} Battery street".format(num))
