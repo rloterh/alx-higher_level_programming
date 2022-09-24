7d164d61157475dbe085ab10973f1385e8a6167e
rloterh <rloterh@gmail.com>
2022-09-24 05:14:43 +0000

    Add a program that imports all functions from the file calculator_1.py

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+from calculator_1 import add, mul, sub, div
+if __name__ == '__main__':
+    from sys import argv
+    if len(argv) != 4:
+        print("Usage: {:s} <a> <operator> <b>".format(argv[0]))
+        exit(1)
+    a = int(argv[1])
+    b = int(argv[3])
+    if argv[2] != '+' and argv[2] != '-' and argv[2] != '*' and argv[2] != '/':
+        print("Unknown operator. Available operators: +, -, *, and /")
+        exit(1)
+    operator = argv[2]
+    if operator == '+':
+        print("{} {} {} = {}".format(a, operator, b, add(a, b)))
+    elif operator == '-':
+        print("{} {} {} = {}".format(a, operator, b, sub(a, b)))
+    elif operator == '*':
+        print("{} {} {} = {}".format(a, operator, b, mul(a, b)))
+    elif operator == '/':
+        print("{} {} {} = {}".format(a, operator, b, div(a, b)))
+        
