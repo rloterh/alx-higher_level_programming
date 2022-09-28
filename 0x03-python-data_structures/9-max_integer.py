e6dc3cd67d626d16f86678336abc30b116e50c58
rloterh <rloterh@gmail.com>
2022-09-28 04:34:43 +0000

    Add a C function that prints some basic info about Python lists

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list is None or len(my_list) is 0:
+    if len(my_list) < 1:
         return None
-    max = my_list[0]
-    for i in range(len(my_list)):
-        if max < my_list[i]:
-            max = my_list[i]
-    return max
+    maximum = my_list[0]
+    for num in my_list:
+        if num > maximum:
+            maximum = num
+    return maximum
