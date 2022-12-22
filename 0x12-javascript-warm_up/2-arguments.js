c0b8150df40426e82be2a8ee396f80ce47e57775
rloterh <rloterh@gmail.com>
2022-12-22 05:33:41 +0000

    Update tasks

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
-if (process.argv.length === 2) {
+const numArgs = process.argv.length - 2;
+if (numArgs === 0) {
   console.log('No argument');
-} else if (process.argv.length === 3) {
+} else if (numArgs === 1) {
   console.log('Argument found');
-} else if (process.argv.length > 3) {
+} else {
   console.log('Arguments found');
 }
