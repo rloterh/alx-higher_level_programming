00fabae77f305165d68daf562d0f6d8f1a6d7598
rloterh <rloterh@gmail.com>
2023-01-18 05:24:18 +0000

    Update task

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,7 +3,7 @@
 import requests
 
 if __name__ == '__main__':
-    got = requests.get("https://intranet.hbtn.io/status")
+    got = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(got.text)))
     print("\t- content: {}".format(got.text))
