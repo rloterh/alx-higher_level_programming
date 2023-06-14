d8761474de02bedc1eb03d132269d192e2afa53d
Robert Loterh <rloterh@gmail.com>
2023-06-14 04:53:35 +0000

    Update task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
 """Add all arguments to a Python list and save them to a file."""
-import sys
+import os
+from sys import argv
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
+dlist = []
+for i in argv:
+    if i == argv[0]:
+        continue
+    dlist += [i]
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+if os.path.exists("add_item.json"):
+    read_info = load_from_json_file("add_item.json")
+    if read_info != dlist:
+        new_list = read_info + dlist
+        save_to_json_file(new_list, "add_item.json")
+        exit(1)
+
+save_to_json_file(dlist, "add_item.json")
