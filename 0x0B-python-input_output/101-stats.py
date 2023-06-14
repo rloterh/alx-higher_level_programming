8709a7759f118245fcabe17d7b16de1d564b4dc3
Robert Loterh <rloterh@gmail.com>
2023-06-14 05:59:37 +0000

    Update task

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,15 +1,45 @@
 #!/usr/bin/python3
-"""Python script to read stdin line by line and compute metrics."""
+"""
+this is all stats
+"""
 import sys
 
-input_line = []
-count_line = 0
-total_size = 0
-for line in sys.stdin:
-    count_line += 1
-    line = line.strip()
-    words = line.split()
-    
+def print_stats(total_size, status_codes):
+    """Print the computed statistics"""
+    print("File size: {}".format(total_size))
+    for code, count in sorted(status_codes.items()):
+        print("{}: {}".format(code, count))
 
-    if count_line % 10 == 0:
-        print(f"File size: {total_size}")
+def parse_line(line):
+    """Parse a line and extract file size and status code"""
+    parts = line.split()
+    if len(parts) >= 9:
+        return int(parts[-1]), parts[-2]
+    return 0, None
+
+def update_stats(stats, size, code):
+    """Update the statistics with new file size and status code"""
+    stats['total_size'] += size
+    if code:
+        if code not in stats['status_codes']:
+            stats['status_codes'][code] = 0
+        stats['status_codes'][code] += 1
+
+try:
+    line_count = 0
+    total_size = 0
+    status_codes = {}
+    for line in sys.stdin:
+        line_count += 1
+        size, code = parse_line(line.strip())
+        total_size += size
+        if code:
+            if code not in status_codes:
+                status_codes[code] = 0
+            status_codes[code] += 1
+
+        if line_count % 10 == 0:
+            print_stats(total_size, status_codes)
+
+except KeyboardInterrupt:
+    print_stats(total_size, status_codes)
