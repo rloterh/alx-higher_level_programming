bb6286bb83f516e5489cca338a733af0434ab589
Robert Loterh <rloterh@gmail.com>
2023-06-14 05:46:35 +0000

    Update tasks

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-"""Module for file input/output"""
+"""A function that writes a object to a text file using a JSON representation.
+"""
 
-import json
 
+def save_to_json_file(my_obj: object, filename: str) -> None:
+    """Write `my_obj` to `filename`.
 
-def save_to_json_file(my_obj, filename):
-    """returns the JSON representation of a string"""
-    with open(filename, 'w') as p:
-        return json.dump(my_obj, p)
+    Args:
+        my_obj: A Python object
+        filename: Name of the file to write to.
+    """
+    import json
+
+    with open(filename, mode='w', encoding='utf-8') as json_file:
+        json.dump(my_obj, json_file)
--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-"""Module for file input/output"""
-
-import json
+"""A function that creates an object from a JSON file."""
 
 
-def load_from_json_file(filename):
-    """returns the JSON representation of a string"""
-    with open(filename, 'r') as p:
-        return json.load(p)
+def load_from_json_file(filename: str) -> object:
+    """Create an object from a JSON file
+
+    Args:
+        filename: Name of the file to write to.
+    """
+    import json
+
+    with open(filename, mode='r', encoding='utf-8') as json_file:
+        return json.load(json_file)
