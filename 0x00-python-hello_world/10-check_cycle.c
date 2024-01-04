0ecd7b261d6f9c0740f0d4cc8ef5aa81f893d4ee
Robert Loterh <rloterh@gmail.com>
2024-01-04 03:33:55 +0000

    Format code

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,12 +1,6 @@
 #include "lists.h"
 #include <stdio.h>
 
-/**
- * check_cycle - checks a linked list for a cycle / loop
- * @list: pointer to linked list
- * Return: int 0 if no loop 1 if loop detected
- */
-
 int check_cycle(listint_t *list)
 {
 	const listint_t *t, *h;
